902da811e3ddcfdfaa9c27edee31565fd302736d
Oliver Widder <oliver.widder@gmail.com>
2017-09-24 09:15:40 +0200

    slideno in URL

---
+++ presentation/step07.js
@@ -0,0 +1,71 @@
+function step07() {
+    const svg = d3.select("#svg07");
+    const selection = svg.selectAll("circle")
+        .data([
+            {name:"a", x:100, y:100, r:20, color:"blue"},
+            {name:"b", x:100, y:400, r:30, color:"orange"},
+            {name:"c", x:600, y:400, r:40, color:"red"}
+        ]);
+
+    selection.enter()
+        .append("circle")
+        .attr("cx", 500)
+        .attr("cy", 300)
+        .attr("r", 5);
+
+    svg.selectAll("circle")
+        .transition()
+        .duration(5000)
+        .attr("cx", function (d) {
+            return d.x;
+        })
+        .attr("cy", function (d) {
+            return d.y;
+        })
+        .attr("r", function (d) {
+            return d.r;
+        })
+        .style("fill", function (d) {
+            return d.color;
+        })
+
+    setTimeout(function () {
+        const selection = svg.selectAll("circle")
+            .data([
+                {name:"c", x:100, y:100, r:80, color:"grey"},
+                {name:"d", x:600, y:100, r:100, color:"green"},
+                {name:"b", x:600, y:400, r:60, color:"cyan"}
+            ], function (d) {
+                return d.name;
+            });
+
+        selection.enter()
+            .append("circle")
+            .attr("cx", 500)
+            .attr("cy", 300)
+            .attr("r", 5);
+
+        svg.selectAll("circle")
+            .transition()
+            .duration(5000)
+            .attr("cx", function (d) {
+                return d.x;
+            })
+            .attr("cy", function (d) {
+                return d.y;
+            })
+            .attr("r", function (d) {
+                return d.r;
+            })
+            .style("fill", function (d) {
+                return d.color;
+            })
+
+        selection.exit()
+            .transition()
+            .duration(5000)
+            .attr("r", 0)
+            .style("opacity", 0)
+            .remove();
+    }, 8000);
+}
---
+++ presentation/step08.js
@@ -0,0 +1,58 @@
+function step08() {
+    const svg = d3.select("#svg08");
+
+    function draw(data) {
+        const selection = svg.selectAll("circle")
+            .data(data, function (d) {
+                return d.name;
+            });
+
+        selection.enter()
+            .append("circle")
+            .attr("cx", 500)
+            .attr("cy", 300)
+            .attr("r", 5);
+
+        svg.selectAll("circle")
+            .transition()
+            .duration(2000)
+            .attr("cx", function (d) {
+                return d.x;
+            })
+            .attr("cy", function (d) {
+                return d.y;
+            })
+            .attr("r", function (d) {
+                return d.r;
+            })
+            .style("fill", function (d) {
+                return d.color;
+            });
+
+        selection.exit()
+            .transition()
+            .duration(2000)
+            .attr("r", 0)
+            .style("opacity", 0)
+            .remove();
+    }
+
+    const color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    setInterval(function () {
+        const data = [];
+        _.range(20).forEach(function(i) {
+            if(_.random(0, 9) > 0) {
+                data.push({
+                    name: i,
+                    x: _.random(0, 1000),
+                    y: _.random(0, 600),
+                    r: _.random(5, 50),
+                    color: color(i)
+                });
+            }
+        });
+
+        draw(data);
+    }, 2000);
+}
